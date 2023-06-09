afba7a2cbda32fbae162c3d3a1444904f0bdd4ce
alinaryzhkova <alinaryzhkova@ALINARYZHKOF873>
2023-06-09 09:23:02 +0300

    Добавьте файлы проекта.

---
+++ deserLib/Class1.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+
+namespace deserLib
+{
+    public class Class1
+    {
+        public static T deserializ<T>(string p)
+        {
+            string json = File.ReadAllText(p);
+            T json_text = JsonConvert.DeserializeObject<T>(json);
+            return json_text;
+        }
+
+        public static void serializ<T>(T items_zametki, string p)
+        {
+            string json = JsonConvert.SerializeObject(items_zametki);
+            File.WriteAllText(p, json);
+        }
+    }
+}
